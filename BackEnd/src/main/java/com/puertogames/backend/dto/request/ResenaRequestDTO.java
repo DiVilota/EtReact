e990d5279689fc381bef9dc9a66a83972b604700
DiVilota <di.villota@duocuc.cl>
2025-12-17 10:44:59 -0300

    Agregado Constructores a ResenaRequest DTO

--- BackEnd/src/main/java/com/puertogames/backend/dto/request/ResenaRequestDTO.java
+++ BackEnd/src/main/java/com/puertogames/backend/dto/request/ResenaRequestDTO.java
@@ -1,13 +1,7 @@
 package com.puertogames.backend.dto.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import jakarta.validation.constraints.*;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ResenaRequestDTO {
 
     @NotNull(message = "El ID del usuario es requerido")
@@ -22,4 +16,49 @@ public class ResenaRequestDTO {
     private Integer rating;
 
     private String comentario;
+
+    // Constructor vacío (OBLIGATORIO para Jackson)
+    public ResenaRequestDTO() {
+    }
+
+    // Constructor con parámetros
+    public ResenaRequestDTO(Long usuarioId, Long juegoId, Integer rating, String comentario) {
+        this.usuarioId = usuarioId;
+        this.juegoId = juegoId;
+        this.rating = rating;
+        this.comentario = comentario;
+    }
+
+    // Getters y Setters
+    public Long getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public Long getJuegoId() {
+        return juegoId;
+    }
+
+    public void setJuegoId(Long juegoId) {
+        this.juegoId = juegoId;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
 }
